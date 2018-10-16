1d440cb62f7e437d5522af1874ce071ebd6c3ffe
kristina <kris580303@gmail.com>
2018-10-16 11:55:36 +0300

    popup ban scroll

--- js/main.js
+++ js/main.js
@@ -5,11 +5,19 @@ $(document).ready(function() {
   $('.map__img').click(function(){
     $('.popup').fadeIn(500);
 
+    $(document.body).css('overflow', 'hidden');
+
     $('.popup__content').click(function(){
       $('.popup').fadeOut(500);
+
+      $(document.body).css('overflow', 'scroll');
+
     });
     $('.popup__close').click(function(){
       $('.popup').fadeOut(500);
+
+      $(document.body).css('overflow', 'scroll');
+      
     });
   });
 });
