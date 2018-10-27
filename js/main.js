c4626a1ade43f8f885cf435d986fce12820347c9
kristina <kris580303@gmail.com>
2018-10-27 11:15:31 +0300

    quotes

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
 $(document).ready(function() {
-  $('.slider').slick({
+  $(".slider").slick({
     dots: true,
   });
   function togglePopup() {
-    $('.popup').toggleClass('hide');
-    $('.popup-shim').toggleClass('hide');
-    $("body").toggleClass('hidden');
+    $(".popup").toggleClass("hide");
+    $(".popup-shim").toggleClass("hide");
+    $("body").toggleClass("hidden");
   };
   function addContentToPopup(selector) {
     $(".popup__content").html($(selector));
   }
-  $('.map__img').click(function(e){
+  $(".map__img").click(function(e){
     e.preventDefault();
     addContentToPopup(".map-google");
     togglePopup();
   });
-  $('.popup-shim').click(togglePopup);
-  $('.popup__close').click(togglePopup);
+  $(".popup-shim").click(togglePopup);
+  $(".popup__close").click(togglePopup);
 });
 
