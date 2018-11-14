7fc6a6ee9a08a12c841fc11f2aa568030e926e5b
0ro <den.bogdanov4@gmail.com>
2018-11-14 09:23:16 +0300

    Fix updating of popup

--- js/main.js
+++ js/main.js
@@ -1,20 +1,21 @@
 $(document).ready(function() {
   $(".slider").slick({
-    dots: true,
+    dots: true
   });
   function togglePopup() {
     $(".popup").toggleClass("hide");
     $(".popup-shim").toggleClass("hide");
     $("body").toggleClass("hidden");
-  };
-  function addContentToPopup(selector) {
-    $(".popup__content").not("map-google").html($(selector));
   }
-  $(".map__img").click(function(e){
+  function addContentToPopup(selector) {
+    if (!$(".popup__content").has(selector).length) {
+      $(".popup__content").html($(selector));
+    }
+  }
+  $(".map__img").click(function(e) {
     e.preventDefault();
     addContentToPopup(".map-google");
     togglePopup();
   });
   $(".popup-shim, .popup__close").click(togglePopup);
 });
-
